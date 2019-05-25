b3ad7301087d5a2d798be876cb246d708191b48a
oksongh <oksongh@gmail.com>
2019-05-25 19:20:26 +0900

    schedule.cppへの移行

--- open_curtain_esp32/curtain/src/schedule.cpp
+++ open_curtain_esp32/curtain/src/schedule.cpp
@@ -7,6 +7,7 @@ long Schedule::to_time(char c1, char c2){
   return (c1 - '0') * 10 + c2 - '0';
 
 }
+
 void Schedule::split(String s){
   if(s.length() == 0){
     return;
@@ -19,6 +20,7 @@ void Schedule::split(String s){
     }
   }
 }
+
 void Schedule::set_time(const char *str, const int len){
   if(len != 4){
     Serial.printf("%d\n",len);
@@ -38,9 +40,59 @@ void Schedule::set_now(const char* str,const int len){
   _now_min = to_time(str[2], str[3]);
 
 }
+
+void Schedule::read_button(){
+
+  if(digitalRead(closepin)){
+    _state |= close;
+    // sche.or_state(Schedule::close);
+  }else if(digitalRead(openpin)){
+    _state |= open;
+    // sche.or_state(Schedule::open);
+  }else{
+
+  }
+}
+
+void Schedule::add_task(){
+  if(_exetime_min == init_time || _exetime_hour == init_time){
+    _exetime_hour = _now_hour;
+    _exetime_min = _now_min;
+  }
+  tasks.push_back(*this);
+}
+
 void Schedule::serialprint(){
   Serial.printf("now %ld:%ld,exe at %ld:%ld,state:%d,state:", _now_hour,_now_min,_exetime_hour,_exetime_min,_state);
   Serial.println(_state,BIN);
 
+}
+void Schedule::parse(String msg){
+  split(msg);
+  for(String s:_vecstr){
+    Serial.printf("_vecstr %s\n", s.c_str());
+  }
 
+  switch (_vecstr.size()) {
+    case 1:
+      if(_vecstr[0] == str_open){
+        _state |= open;
+
+      }else if(_vecstr[0] == str_close){
+        _state |= close;
+
+      }else{
+
+      }
+    case 2:
+      if(_vecstr[0] == str_reserve){
+        // etc 1220,0930,2310
+        set_time(_vecstr[1].c_str(), _vecstr[1].length());
+
+      }else if(_vecstr[0] == str_now){
+        set_now(_vecstr[1].c_str(),_vecstr[1].length());
+
+      }else{
+      }
+  }
 }
--- open_curtain_esp32/curtain/src/schedule.h
+++ open_curtain_esp32/curtain/src/schedule.h
@@ -21,11 +21,8 @@ private:
   std::vector<String> _vecstr;
 
   long to_time(char c1,char c2);
-
   void split(String s);
-
   void set_time(const char* str,const int len);
-
   void set_now(const char* str,const int len);
 
 public:
@@ -39,61 +36,9 @@ public:
     return _state;
   }
 
-  void parse(String msg){
-    split(msg);
-    for(String s:_vecstr){
-      Serial.printf("_vecstr %s\n", s.c_str());
-    }
-
-    switch (_vecstr.size()) {
-      case 1:
-
-        if(_vecstr[0] == str_open){
-          _state |= open;
-
-        }else if(_vecstr[0] == str_close){
-          _state |= close;
-
-        }else{
-
-        }
-
-      case 2:
-
-        if(_vecstr[0] == str_reserve){
-          // etc 1220,0930,2310
-          set_time(_vecstr[1].c_str(), _vecstr[1].length());
-
-        }else if(_vecstr[0] == str_now){
-          set_now(_vecstr[1].c_str(),_vecstr[1].length());
-
-        }else{
-
-        }
-    }
-  }
-  void read_button(){
-
-    if(digitalRead(closepin)){
-      _state |= close;
-      // sche.or_state(Schedule::close);
-    }else if(digitalRead(openpin)){
-      _state |= open;
-      // sche.or_state(Schedule::open);
-    }else{
-
-    }
-  }
-
-
-  void add_task(){
-    if(_exetime_min == init_time || _exetime_hour == init_time){
-      _exetime_hour = _now_hour;
-      _exetime_min = _now_min;
-    }
-    tasks.push_back(*this);
-  }
-
+  void parse(String msg);
+  void read_button();
+  void add_task();
   void serialprint();
 
 };
